150076ae95dc722f4763b9966f8cd89ac54abb7f
kiyotaki4 <marlinezxc@gmail.com>
2025-04-04 09:02:26 +0900

    fixed issue and updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import store from './app/store'
-import { Provider } from 'react-redux'
-import { Counter } from './features/counter/Counter.jsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './About.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import store from './app/store';
+import App from './App.jsx';
+import About from './About.jsx';
+import { Counter } from './features/counter/Counter.jsx';
+import './index.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <nav>
-          <ul>
-            <li><a href="/">Главная</a></li>
-            <li><a href="/about">Контакты</a></li>
-          </ul>
-        </nav>
     <Router>
-    <Routes>
-          <Route path="/" element={<Provider store={store}>
-      <Counter />
-      <App />
-  </Provider>} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-    
-  </Router>
-  </StrictMode>,
-)
+      <nav>
+        <ul>
+          <li><Link to="/">Главная</Link></li>
+          <li><Link to="/about">Контакты</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Provider store={store}>
+              <Counter />
+              <App />
+            </Provider>
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
